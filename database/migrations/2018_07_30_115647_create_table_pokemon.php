e466d338e2d2405fe8d934145c793791826bd140
Rafaell Lycan <sonny.webdsg@gmail.com>
2018-07-30 14:55:55 +0200

    Add Lumen project

---
+++ database/migrations/2018_07_30_115647_create_table_pokemon.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablePokemon extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pokemon', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('number')->unique();
+            $table->string('name');
+            $table->string('description');
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pokemon');
+    }
+}
